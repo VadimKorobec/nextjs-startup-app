681220262e806fc907b56454abc111768f6f93d2
Vadim <_s_e_t__@ukr.net>
2025-06-04 17:40:42 +0300

    add components folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+export const Layout = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  <main className=" font-work-sans">{children}</main>;
+};
+
+
+export default Layout
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.username === "admin" &&
+          credentials?.password === "1234"
+        ) {
+          return { id: "1", name: "Admin", email: "admin@example.com" };
+        }
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/signin", // если хочешь кастомную страницу входа
+  },
+});
+
+export { handler as GET, handler as POST };
